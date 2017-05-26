d6cfb602fe914b144f6b7cfb69905e86250d34f8
DennisKimani <dennisngugi.dk@gmail.com>
2017-05-26 17:39:26 +0300

    tomorrow will retry

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "44986d9e9bb194c6b44123cf12ae2c76e119861c";
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepository = function(){
+  $.get('https://api.github.com/users/ + ?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+exports.repositoryModule = repository;
+
+},{"./../.env":1}],3:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+var repository = require('./../js/repo.js').repositoryModule;
+
+$(document).ready(function() {
+  $('#what').click(function() {
+    var project = $('#yeah').val();
+    $('#yeah').val("");
+  });
+});
+
+},{"./../.env":1,"./../js/repo.js":2}]},{},[3]);
--- js/repo.js
+++ js/repo.js
@@ -1,7 +1,10 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + '44986d9e9bb194c6b44123cf12ae2c76e119861c').then(function(response){
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepository = function(){
+  $.get('https://api.github.com/users/ + ?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+exports.repositoryModule = repository;
