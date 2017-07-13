addffb44abd40a314f595e6a00303303420ee8c7
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-13 09:52:35 +0300

    removed the console log functions

--- js/repo.js
+++ js/repo.js
@@ -9,20 +9,16 @@ findthem = function(){
 };
 
 repository.prototype.getrepository = function(name,displayFunction){
-  console.log(name);
   $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(response){
     displayFunction(response.name);
-    console.log(response);
   }).fail(function(error) {
     console.log('error');
   });
 };
 
 findthem.prototype.getfindthem = function(repos,displayFunction){
-  console.log(repos);
-  $.get('https://api.github.com/users/' + repos + '/repos/?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + name + '/repos/?access_token=' + apiKey).then(function(response){
     displayFunction(response.repos);
-    console.log(response);
   }).fail(function(error) {
     console.log('error');
   });
